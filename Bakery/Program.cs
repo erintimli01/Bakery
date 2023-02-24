87b975ef3c92fb0a40bbfe3d221bbe8eab632793
Erin Timlin <erintimlin@gmail.com>
2023-02-24 16:19:20 -0500

    UI WIP

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using Bakery.Models;
 
-class Program
+namespace Bakery.UI
 {
-  static void Main()
+
+  class Program
   {
+    static void Main(string[] welcome)
+    {
+      Console.WriteLine("Welcome to Pierre's Perfect Pastries and Bread Bakery!");
+      Console.WriteLine("We sell perfect pastries for $2 each or buy 3, get 1 free and perfect bread for $5 each or buy 2, get 1 free!");
+
+      Console.WriteLine("How many loaves of bread would you like to take home with you?");
+      int breadQuantity = int.Parse(Console.ReadLine());
+
+    }
    //UI goes here
   }
+}
 }

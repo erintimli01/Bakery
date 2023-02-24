082c5ff6e18042febafce19986a6061367020f97
Erin Timlin <erintimlin@gmail.com>
2023-02-24 14:42:04 -0500

    add pastry files and test passed for instance of pastry

---
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+
+namespace Bakery.Tests
+{
+[TestClass]
+  public class PastryTests
+  {
+    [TestMethod]
+    public void CreateInstance_CreateInstanceOfPastry_Pastry()
+    {
+      Pastry pastryInstance = new Pastry();
+      Assert.AreEqual(typeof(Pastry), pastryInstance.GetType());
+    }
+  }
+}
